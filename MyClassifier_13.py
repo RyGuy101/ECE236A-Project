8ffeba78b8525b3fc2cdbe4d0e7b1dce9571472a
Ryan Nemiroff <ryguyn@gmail.com>
2021-10-17 14:30:26 -0700

    first commit

---
+++ MyClassifier_13.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class MyClassifier:
+    def __init__(self):
+        pass
+
+    def sample_selection(self, training_sample):
+        return self
+
+    def train(self, train_data, train_label):
+        return self
+
+    def f(self, input):
+        pass
+
+    def test(self, test_data):
+        pass
