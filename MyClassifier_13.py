39cd136b7119f74ef7201494bff896c60451435b
Ryan Nemiroff <ryguyn@gmail.com>
2021-11-06 01:45:33 -0700

    Notation & LP updates

--- MyClassifier_13.py
+++ MyClassifier_13.py
@@ -15,12 +15,17 @@
 #    minimize the sum of all [max{0, 1 - s_i((a^T)x_i + b)} + lambda*|a|^2]
 #    Question: What is lambda? Looks like it could be a hyperparameter we choose.
 
-# Notation in Project Description:
+# Notation in Project Description (used in the code):
 #     Training data has samples i = 1,...,N
 #     y_i is data point (length M vector), s_i is label (+1 or -1)
 #     W (M by L matrix) is weights, w (length L vector) is bias (L is chosen by us)
 #     g(y) = (W^T)y + w
 #     f(x) = f(g(y)) decides classification label
+#
+# Example LP for L = 1:
+#     minimize the sum of all t_i
+#     s.t. 0 <= t_i, i=1,..,N
+#          1 - s_i*((W^T)y_i + w) <= t_i
 
 #######
 # this may be useful for svm implementation:  https://www.cvxpy.org/examples/machine_learning/svm.html
@@ -39,37 +44,36 @@ class MyClassifier:
     
     Attributes:
 
-        X_train: the training data vectors
-        Y_train: the training data labels
-        X_test: the testing data vectors
-        Y_test: the testting data labels
+        y_train: the training data vectors
+        s_train: the training data labels
+        y_test: the testing data vectors
+        s_test: the testting data labels
         
-        n: how many dimensions our data input is
+        M: how many dimensions our data input is
             e.g. the MNIST would have 784 as n, as it is a 28x28 image
-        m: how many training samples we have
-        a: the weight vector [n x 1]
-        b: the bias vector [1]
+        N: how many training samples we have
+        W: the weight vector [M x 1]
+        w: the bias vector [1]
         lambda: a hyperparameter (TODO: tune this)
     
     """
     def __init__(self, data):
         """initializes the data
         arg:
-            data: a list consisting of [X_train, y_train, X_test, y_test]
+            data: a list consisting of [y_train, s_train, y_test, s_test]
         """
         np.random.seed(1) # for reproducibility
         
-        self.X_train = data[0]
-        self.y_train = data[1]
-        self.X_test = data[2]
-        self.y_test = data[3]
+        self.y_train = data[0]
+        self.s_train = data[1]
+        self.y_test = data[2]
+        self.s_test = data[3]
                       
         # initializing weights
-        self.n = self.X_train.shape[1] #I think m and n should be reversed...? I wrote the train function assuming they are reversed
-        self.m = self.X_train.shape[0]
-        self.a = np.zeros(self.n)
-        self.b = 0
-        self.lambda = 1e-1
+        self.M = self.y_train.shape[1]
+        self.W = np.zeros(self.M)
+        self.w = 0
+        # self.lambda = 1e-1
 
     def sample_selection(self, training_sample):
         if True: # TODO
@@ -92,20 +96,24 @@ class MyClassifier:
 
         '''
         # https://www.cvxpy.org/examples/basic/linear_program.html
-        x = train_data
-        s = train_label
-        t = cp.variable(self.m)
-        prob = cp.Problem(cp.Minimize(np.ones(self.m)@t)),
-                          [np.zeros(self.m) < t ,
-                           np.ones(self.m) - s*(x.T@self.a) + self.b*np.ones(self.m) < t])
+        N_train = train_data.shape[0]
+        Y = train_data
+        S = train_label
+        W = cp.variable(self.M) # Assumes L = 1
+        w = cp.variable(1)
+        t = cp.variable(N_train)
+        prob = cp.Problem(cp.Minimize(np.ones(N_train)@t), [
+            np.zeros(N_train) <= t, # 0 <= t_i, i=1,..,N
+            np.ones(N_train) - S*(Y@W + w*np.ones(N_train)) <= t # 1 - s_i*((W^T)y_i + w) <= t_i
+        ])
         prob.solve()
         print("\nThe optimal value is", prob.value)
-        print("A solution t is")
-        print(t.value)
+        print("A solution W, w is")
+        print("W = {}".format(W))
+        print("w = {}".format(w))
 
-        #How to get a, b values from t?
-
-
+        self.W = W
+        self.w = w
         return self
 
     def f(self, input):
@@ -113,30 +121,33 @@ class MyClassifier:
 
         Args:
             input: vector of length L
-                   corresponds to the function g(y)
+                   corresponds to the function f(x) = f(g(y))
 
         Returns:
             estimated class
 
         '''
 
-        if np.dot(self.a, input) + self.b > 0:
+        if abs(input) < 1:
+            print("Unsure about classification. Value is {}".format(input))
+
+        # decision function in Project Description
+        if input > 0:
             return 1
-        elif np.dot(self.a, input) + self.b <= 1:
+        elif input <= 0:
             return -1
 
     def test(self, test_data):
         '''
 
         Args:
-            test_data: m_test x n_test size matrix where
-                       m_test: number of features
-                       n_test: number of test data
+            test_data: N_test x M size matrix where
+                       M: number of features
+                       N_test: number of test data
 
         Returns:
             vector that contains the classification decisions
 
         '''
-        return np.apply_along_axis(self.f, 0, test_data)
-
-
+        N_test = test_data.shape[0]
+        return np.apply_along_axis(self.f, 0, test_data@self.W + self.w*np.ones(N_test))
