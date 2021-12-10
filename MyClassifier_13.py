de3e038b8b5a5885b511d8314495e51735632d8a
Hayato Kato <hayatokato8810@gmail.com>
2021-12-09 16:01:42 -0800

    Refined Generated Figures

--- MyClassifier_13.py
+++ MyClassifier_13.py
@@ -36,6 +36,10 @@ import numpy as np
 import cvxpy as cp
 import matplotlib.pyplot as plt
 
+import csv
+
+filename = './dataset/test/mnist_test_17.csv'
+
 ### helper functions:
 
 ### MyClassifier Class
@@ -72,15 +76,36 @@ class MyClassifier:
         # initialize empty training set
         self.y_train = np.zeros((0, self.M))
         self.s_train = np.zeros(0, dtype=np.int8)
-
-    def sample_selection(self, training_sample):
-        if True: # TODO
-            self.y_train = np.append(self.y_train, training_sample)
-            self.s_train = np.append(self.s_train, self.current_training_label) # set as a class member since it is not a function input
+        self.linearly_separable = True # assume linearly separable data to start
         
+    def sample_selection(self, training_sample, training_label): # training_label added as an input, as mentioned on Campuswire
+        '''
+
+        Args:
+            training_sample: vector of length M
+                        M: number of features in data vectors (784) for MNIST
+            training_label: class label (1 or -1)
+
+        Returns:
+            MyClassifier object
+
+        '''
+
+        g_y = training_sample@self.W + self.w
+        penalty = 1 - (training_label * g_y)
+
+        # penalty <= 0     -> classified perfectly correctly
+        # 0 < penalty < 1  -> classified correctly but within margin
+        # penalty >= 1     -> classified incorrectly
+
+        # Select points that are classified incorrectly, or classified correctly and some distance within the margin (need to include some correct classifications to balance out noisy data (e.g. synthetic dataset))
+        if penalty >= 0.3125:
+            self.y_train = np.append(self.y_train, [training_sample], axis=0)
+            self.s_train = np.append(self.s_train, [training_label], axis=0)
+
         return self
 
-    def train(self, train_data, train_label):
+    def train(self, train_data=None, train_label=None):
         '''
 
         Args:
@@ -94,20 +119,48 @@ class MyClassifier:
             MyClassifier object
 
         '''
+        if train_data is None:
+            train_data = self.y_train
+        if train_label is None:
+            train_label = self.s_train
+
         # https://www.cvxpy.org/examples/basic/linear_program.html
         N_train = train_data.shape[0]
         Y = train_data
         S = train_label
+
+        if not np.any(S == 1) or not np.any(S == -1):
+            return # Do nothing. Need both classes to train effectively
+
         W = cp.Variable(self.M) # Assumes L = 1
         w = cp.Variable(1)
-        t = cp.Variable(N_train)
-        prob = cp.Problem(cp.Minimize(np.ones(N_train)@t), [
-            np.zeros(N_train) <= t, # 0 <= t_i, i=1,..,N
-            1 - (Y[S == 1]@W + w) <= t[S == 1], # 1 - s_i*((W^T)y_i + w) <= t_i
-            1 + (Y[S == -1]@W + w) <= t[S == -1]
-        ])
-        prob.solve()
-        print("\nThe optimal value is", prob.value)
+
+        if self.linearly_separable:
+            s = cp.Variable(self.M)
+            r = cp.Variable(1)
+            prob = cp.Problem(cp.Minimize((1+1/np.sqrt(self.M))*np.ones(self.M)@s + (1+np.sqrt(self.M))*r), [ # approximation of euclidean norm
+                W <= s,
+                W >= -s,
+                W <= r,
+                W >= -r,
+                1 - (Y[S == 1]@W + w) <= 0,
+                1 + (Y[S == -1]@W + w) <= 0
+            ])
+            prob.solve()
+            if prob.status == "infeasible":
+                self.linearly_separable = False
+                print("Data is not linearly separable")
+        
+        if not self.linearly_separable:
+            t = cp.Variable(N_train)
+            prob = cp.Problem(cp.Minimize(np.ones(N_train)@t), [
+                np.zeros(N_train) <= t, # 0 <= t_i, i=1,..,N
+                1 - (Y[S == 1]@W + w) <= t[S == 1], # 1 - s_i*((W^T)y_i + w) <= t_i
+                1 + (Y[S == -1]@W + w) <= t[S == -1]
+            ])
+            prob.solve()
+        
+        # print("\nThe optimal value is", prob.value)
         # print("A solution W, w is")
         # print("W = {}".format(W.value))
         # print("w = {}".format(w.value))
@@ -134,8 +187,10 @@ class MyClassifier:
         # decision function in Project Description
         if input > 0:
             return 1
-        elif input <= 0:
+        elif input < 0:
             return -1
+        else:
+            return np.random.choice([1, -1])
 
     def test(self, test_data):
         '''
